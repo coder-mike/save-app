367c4831d83e4f4ab365407035bb4566acd376ac
Michael Hunter <elec.mike@gmail.com>
2021-04-05 17:18:26 +1000

    More error reporting in API

--- api.php
+++ api.php
@@ -2,6 +2,10 @@
 
 require_once "config.php";
 
+mysqli_report(MYSQLI_REPORT_ALL);
+
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
 $req = json_decode(file_get_contents('php://input'), true);
 
 $res = array('success' => false, 'reason' => 'Unknown reason');
@@ -22,6 +26,11 @@ header('Content-Type: application/json');
 echo json_encode($res);
 mysqli_close($link);
 
+function logError($err) {
+  error_log($err);
+  // echo $err;
+}
+
 function errorResponse($reason) {
   global $res;
   $res = array('success' => false, 'reason' => $reason);
@@ -40,11 +49,13 @@ function newAccount() {
 
   $stmt = mysqli_prepare($link, "SELECT id FROM users WHERE email = ?");
   if (!$stmt) {
+    logError('newAccount: failed to prepare SQL statement to fetch user');
     return;
   }
 
   mysqli_stmt_bind_param($stmt, "s", $email);
   if (!mysqli_stmt_execute($stmt)) {
+    logError('newAccount: failed to bind params to fetch user');
     mysqli_stmt_close($stmt);
     return;
   }
@@ -59,8 +70,13 @@ function newAccount() {
   mysqli_stmt_close($stmt);
 
   $stmt = mysqli_prepare($link, "INSERT INTO users (id, name, email, password, state) VALUES (UUID(), ?, ?, ?, ?)");
+  if (!$stmt) {
+    logError('newAccount: failed to prepare SQL statement to insert new user');
+    return;
+  }
   mysqli_stmt_bind_param($stmt, "ssss", $name, $email, $password_hash, $state);
   if (!mysqli_stmt_execute($stmt)) {
+    logError('newAccount: failed to execute SQL statement to insert new user');
     return;
   }
   mysqli_stmt_close($stmt);
@@ -79,11 +95,13 @@ function login() {
 
   $stmt = mysqli_prepare($link, "SELECT HEX(id) as id, name, password, state FROM users WHERE email = ?");
   if (!$stmt) {
+    logError('login: failed to prepare SQL statement to select user');
     return;
   }
 
   mysqli_stmt_bind_param($stmt, "s", $email);
   if (!mysqli_stmt_execute($stmt)) {
+    logError('login: failed to execute SQL to select user');
     mysqli_stmt_close($stmt);
     return;
   }
@@ -127,8 +145,13 @@ function save() {
   $state = json_encode($data['state']);
 
   $stmt = mysqli_prepare($link, 'UPDATE users set state = ? WHERE id = (UNHEX(REPLACE(?, "-","")))');
+  if (!$stmt) {
+    logError('save: failed to prepare SQL statement to update user');
+    return;
+  }
   mysqli_stmt_bind_param($stmt, "ss", $state, $userId);
   if (!mysqli_stmt_execute($stmt)) {
+    logError('save: failed to execute statement to update user');
     return;
   }
 
@@ -148,8 +171,13 @@ function load() {
   $userId = $data['userId'];
 
   $stmt = mysqli_prepare($link, 'SELECT HEX(id) as id, name, email, state FROM users WHERE id = (UNHEX(REPLACE(?, "-","")))');
+  if (!$stmt) {
+    logError('load: failed to prepare SQL statement');
+    return;
+  }
   mysqli_stmt_bind_param($stmt, "s", $userId);
   if (!mysqli_stmt_execute($stmt)) {
+    logError('load: failed to execute SQL statement');
     mysqli_stmt_close($stmt);
     return;
   }
