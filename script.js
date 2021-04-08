7df0115f8c8066d54559ff669787fe0f6c03eda1
Michael Hunter <elec.mike@gmail.com>
2021-04-09 05:53:02 +1000

    WIP implementing actions

--- script.js
+++ script.js
@@ -262,7 +262,7 @@ function renderTotals(state) {
   tr = table.appendChild(document.createElement('tr'));
   tr.appendChild(document.createElement('td')).textContent = 'Total available:';
 
-  const lastCommitTime = parseDate(state.time);
+  const lastCommitTime = deserializeDate(state.time);
   const totalSavedEl = tr.appendChild(document.createElement('td'))
     .appendChild(document.createElement('span'));
   totalSavedEl.classList.add('currency');
@@ -304,7 +304,11 @@ function renderList(list) {
   const heading = listNameSection.appendChild(document.createElement('h1'));
   heading.id = 'list-heading';
   heading.classList.add('list-heading')
-  makeEditable(heading, { obj: list, field: 'name' });
+  makeEditable(heading, {
+    obj: list,
+    field: 'name',
+    write: value => foldAction(window.state, { type: 'ListSetName', newName: value })
+  });
 
   // Header info section
   const infoEl = listHeaderEl.appendChild(document.createElement('div'));
@@ -474,7 +478,7 @@ function renderItem(item) {
   const etaEl = infoSectionEl.appendChild(document.createElement('span'));
   etaEl.classList.add('eta');
   const etaStr = item.expectedDate
-    ? formatDate(parseDate(item.expectedDate))
+    ? formatDate(deserializeDate(item.expectedDate))
     : 'Ready';
   etaEl.appendChild(document.createTextNode(etaStr));
 
@@ -631,15 +635,44 @@ function finishedUserInteraction(requiresRender = true) {
   save();
 }
 
-// Updates the state to the latest projected values and sets a timeout to repeat
-// automatically the next time that the state needs to change
+// Updates (projects) the state to the latest projected values and sets a
+// timeout to repeat automatically the next time that the state needs to change
 function update() {
-  let state = window.state;
+  const toTime = Date.now();
 
-  const newTime = Date.now();
+  const { timeOfNextNonlinearity } = project(window.state, toTime);
 
+  window.state = state;
+  window.nextNonlinearity = timeOfNextNonlinearity;
+  window.lastCommitTime = toTime;
+
+
+  if (timeOfNextNonlinearity) {
+    let timeoutPeriod = timeOfNextNonlinearity - Date.now();
+    console.log(`Next nonlinearity in ${timeoutPeriod/1000}s`)
+
+    window.nextNonLinearityTimer && clearTimeout(window.nextNonLinearityTimer);
+    if (timeoutPeriod > 2147483647)
+      timeoutPeriod = 2147483647
+    if (timeoutPeriod < 1)
+      timeoutPeriod = 1;
+    window.nextNonLinearityTimer = setTimeout(() => {
+      if (window.isEditing) {
+        console.log('Not updating at nonlinearity because user is busy editing')
+        return;
+      }
+      console.log('Updating at nonlinearity', formatDate(Date.now()))
+      update(window.state);
+      render();
+    }, timeoutPeriod)
+  }
+}
+
+// Projects the waterfall model to the future time `toTime`. Mutates `state` and
+// returns { timeOfNextNonlinearity }
+function project(state, toTime) {
   state ??= {};
-  state.time ??= serializeDate(newTime);
+  state.time ??= serializeDate(toTime);
   state.nextNonlinearity ??= null;
   state.lists ??= [];
   state.id ??= uuidv4();
@@ -651,7 +684,7 @@ function update() {
   state.currentListIndex = Math.max(state.currentListIndex, 0);
   state.currentListIndex = Math.min(state.currentListIndex, state.lists.length - 1);
 
-  const lastCommitTime = parseDate(state.time);
+  const lastCommitTime = deserializeDate(state.time);
   let timeOfNextNonlinearity = null;
 
   for (const list of state.lists) {
@@ -668,7 +701,7 @@ function update() {
     let timeCursor = lastCommitTime;
 
     // The amount of money we have left over at `timeCursor`
-    let remainingMoneyToAllocate = list.overflow.value + rateInDollarsPerMs(allocatedRate) * (newTime - lastCommitTime);
+    let remainingMoneyToAllocate = list.overflow.value + rateInDollarsPerMs(allocatedRate) * (toTime - lastCommitTime);
 
     // Rate of change of remainingMoneyToAllocate at `timeCursor`, which
     // eventually gets attributed to the overflow bucket
@@ -735,34 +768,12 @@ function update() {
     list.overflow.rate = overflowRate - debtRate;
   }
 
-  state.time = serializeDate(newTime);
+  state.time = serializeDate(toTime);
   state.nextNonlinearity = timeOfNextNonlinearity
     ? serializeDate(timeOfNextNonlinearity)
     : null;
 
-  window.state = state;
-  window.nextNonlinearity = timeOfNextNonlinearity;
-  window.lastCommitTime = newTime;
-
-  if (timeOfNextNonlinearity) {
-    let timeoutPeriod = timeOfNextNonlinearity - Date.now();
-    console.log(`Next nonlinearity in ${timeoutPeriod/1000}s`)
-
-    window.nextNonLinearityTimer && clearTimeout(window.nextNonLinearityTimer);
-    if (timeoutPeriod > 2147483647)
-      timeoutPeriod = 2147483647
-    if (timeoutPeriod < 1)
-      timeoutPeriod = 1;
-    window.nextNonLinearityTimer = setTimeout(() => {
-      if (window.isEditing) {
-        console.log('Not updating at nonlinearity because user is busy editing')
-        return;
-      }
-      console.log('Updating at nonlinearity', formatDate(Date.now()))
-      update(window.state);
-      render();
-    }, timeoutPeriod)
-  }
+  return { timeOfNextNonlinearity };
 }
 
 function getAllocatedRate(allocated) {
@@ -1002,7 +1013,7 @@ function serializeDate(date) {
     : new Date(date).toISOString();
 }
 
-function parseDate(date) {
+function deserializeDate(date) {
   return date === 'never'
     ? Infinity
     : Date.parse(date)
@@ -1029,15 +1040,12 @@ function windowBlurEvent() {
   hideMenu();
 }
 
-function makeEditable(el, { obj, field, readTransform, writeTransform, requiresRender }) {
+function makeEditable(el, { obj, field, readTransform, writeTransform, read, write, requiresRender }) {
   requiresRender ??= true;
   writeTransform ??= v => v;
   readTransform ??= v => v;
-  const read = () => readTransform(obj[field]);
-  const write = value => {
-    obj[field] = writeTransform(value);
-    obj[field + 'Modified'] = serializeDate(Date.now());
-  }
+  read ??= () => readTransform(obj[field]);
+  write ??= value => obj[field] = writeTransform(value);
 
   el.setAttribute('contentEditable', true);
   el.addEventListener('focus', focus)
@@ -1112,14 +1120,13 @@ function endEdit(changed = true, requiresRender = true) {
 }
 
 function newListClick() {
-  update();
-
-  let newListName = 'Wish list';
+  let name = 'Wish list';
   let counter = 1;
-  while (window.state.lists.some(l => l.name === newListName))
-    newListName = `Wish list ${++counter}`;
+  while (window.state.lists.some(l => l.name === name))
+    name = `Wish list ${++counter}`;
 
-  window.state.lists.push({ name: newListName });
+  foldAction(window.state, { type: 'ListNew', name });
+
   window.state.currentListIndex = window.state.lists.length - 1;
 
   finishedUserInteraction();
@@ -1352,11 +1359,9 @@ function toggleMenu(menu) {
 }
 
 function deleteListClick(event) {
-  update();
-
   const list = event.target.closest('.list').list;
-  const lists = window.state.lists;
-  lists.splice(lists.indexOf(list), 1);
+
+  foldAction(window.state, { type: 'ListDelete', listId: list.id })
   window.state.currentListIndex--;
 
   finishedUserInteraction();
@@ -1626,3 +1631,70 @@ function uuidv4() {
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
   );
 }
+
+// Mutates `state` to include the effect of the given action
+function foldAction(state, action) {
+  action.time ??= serializeDate(Date.now());
+  const time = deserializeDate(action.time);
+
+  project(state, time);
+
+  // For backwards compatibility with states that weren't created using an actions list
+  if (!state.actions) {
+    state.actions = [];
+    state.hash = 'd41d8cd98f00b204e9800998ecf8427e';
+    foldAction(state, { type: 'InitialState', state });
+  }
+
+  state.actions.push(action);
+
+  // Like a git hash, if "actions" are like commits. The hash allows us to
+  // quickly merge two histories or determine if there's a conflict (a fork in
+  // the history). Note that "JSON.stringify" here is technically not correct
+  // since it's not guaranteed to produce a canonical/deterministic output each
+  // time across different environments, but in this case it's safe for it to
+  // produce a different hash for the same history since it will just fall back
+  // to a full history merge (the hash is just used as an optimization).
+  state.hash = md5Hash(JSON.stringify([state.hash, action]));
+
+  switch (action.type) {
+    case 'InitialState':
+      state = JSON.parse(JSON.stringify(action.state));
+      break;
+    case 'ListNew':
+      state.lists.push({ name: action.name });
+      break;
+    case 'ListDelete':
+      const index = lists.findIndex(list => list.id = action.listId);
+      if (index != -1) state.lists.splice(index, 1);
+      break;
+    case 'ListSetName':
+      break;
+    case 'ListInjectMoney':
+      break;
+    case 'ItemNew':
+      break;
+    case 'ItemReorder':
+      break;
+    case 'ItemDelete':
+      break;
+    case 'ItemSetName':
+      break;
+    case 'ItemSetNote':
+      break;
+    case 'ItemSetPrice':
+      break;
+    case 'ItemPurchase':
+      break;
+    case 'ItemRedistributeMoney':
+      break;
+  }
+
+  // Run another projection just to update any side effects of the action. For
+  // example, redistributing newly-available cash
+  return project(state, time);
+}
+
+// https://stackoverflow.com/a/33486055
+function md5Hash(d){var r = M(V(Y(X(d),8*d.length)));return r.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_}
+
