55a13399aaff720dc795cf389c56349c988ee06b
Michael Hunter <elec.mike@gmail.com>
2021-04-10 17:44:47 +1000

    Some bug fixes

--- script.js
+++ script.js
@@ -96,6 +96,12 @@ function render() {
   window.currentListIndex = Math.max(window.currentListIndex, 0);
   window.currentListIndex = Math.min(window.currentListIndex, window.state.lists.length - 1);
 
+  // To prove to ourselves that we can rebuild the state from just the actions (this can be removed when we have confidence)
+  // TODO: Remove this at some point
+  if (window.state.actions) {
+    //window.state = rebuildStateFromActions(window.state.id, window.state.actions);
+  }
+
   saveScrollPosition();
   document.body.replaceChildren(renderPage(window.state))
   restoreScrollPosition();
@@ -321,8 +327,7 @@ function renderList(list) {
   heading.id = 'list-heading';
   heading.classList.add('list-heading')
   makeEditable(heading, {
-    obj: list,
-    field: 'name',
+    read: () => list.name,
     write: value => foldAction(window.state, { type: 'ListSetName', listId: list.id, newName: value })
   });
 
@@ -338,8 +343,7 @@ function renderList(list) {
   const allocatedAmountEl = allocatedEl.appendChild(document.createElement('div'));
   allocatedAmountEl.classList.add('allocated-amount');
   makeEditable(allocatedAmountEl, {
-    obj: list.budget,
-    field: 'dollars',
+    read: () => formatCurrency(list.budget.dollars),
     write: v => foldAction(window.state, { type: 'ListSetBudget', listId: list.id, budget: { dollars: parseNonNegativeCurrency(v), unit: '/month' } })
   });
 
@@ -446,10 +450,9 @@ function renderItem(item) {
   const nameEl = nameSectionEl.appendChild(document.createElement('div'));
   nameEl.classList.add('item-name');
   makeEditable(nameEl, {
-    obj: item,
-    field: 'name',
+    read: () => item.name,
+    write: name => foldAction(window.state, { type: 'ItemSetName', itemId: item.id, name }),
     requiresRender: false,
-    write: name => foldAction(window.state, { type: 'ItemSetName', itemId: item.id, name })
   });
 
   // Item note
@@ -474,9 +477,7 @@ function renderItem(item) {
   // Item Price
   const priceEl = infoSectionEl.appendChild(document.createElement('span'));
   makeEditable(priceEl, {
-    obj: item,
-    field: 'price',
-    readTransform: formatCurrency,
+    read: () => formatCurrency(item.price),
     write: v => foldAction(window.state, {
       type: 'ItemSetPrice',
       itemId: item.id,
@@ -579,7 +580,6 @@ function renderAmount(amount) {
     return amountSpan;
   }
 
-
   const amountSpan = document.createElement('span');
   amountSpan.id = generateNewId();
   amountSpan.classList.add('money');
@@ -657,10 +657,6 @@ function updateState() {
   if (state.lists.length < 1)
     foldAction(state, { type: 'ListNew', id: uuidv4(), name: 'Wish list' });
 
-  // To prove to ourselves that we can rebuild the state from just the actions (this can be removed when we have confidence)
-  if (window.state.actions)
-    window.state = rebuildState(window.state.id, window.state.actions);
-
   const { timeOfNextNonlinearity } = project(window.state, toTime);
 
   window.state = state;
@@ -694,7 +690,7 @@ function project(state, toTime) {
   console.assert(state);
   console.assert(Array.isArray(state.lists));
 
-  state.time ??= toTime;
+  state.time ??= serializeDate(toTime);
 
   const lastCommitTime = deserializeDate(state.time);
   let timeOfNextNonlinearity = null;
@@ -1041,12 +1037,10 @@ function windowBlurEvent() {
   hideMenu();
 }
 
-function makeEditable(el, { obj, field, readTransform, writeTransform, read, write, requiresRender }) {
+function makeEditable(el, { read, write, requiresRender }) {
   requiresRender ??= true;
-  writeTransform ??= v => v;
-  readTransform ??= v => v;
-  read ??= () => readTransform(obj[field]);
-  write ??= value => obj[field] = writeTransform(value);
+  console.assert(read);
+  console.assert(write);
 
   el.setAttribute('contentEditable', true);
   el.addEventListener('focus', focus)
@@ -1220,7 +1214,7 @@ function itemDrop(event) {
   foldAction(window.state, {
     type: 'ItemMove',
     itemId: sourceItem.id,
-    targetList: list.id,
+    targetListId: list.id,
     targetIndex: list.items.indexOf(targetItem),
   });
 
@@ -1731,7 +1725,7 @@ function foldAction(state, action) {
     }
     case 'ListSetBudget': {
       const list = findList(action.listId);
-      if (list) list.budget = action.budget;
+      if (list) Object.assign(list.budget, action.budget);
       break;
     }
     case 'ListInjectMoney': {
@@ -1748,8 +1742,9 @@ function foldAction(state, action) {
       const targetList = findList(action.targetListId);
       if (!source || !targetList) break;
 
-      const sourceIndex = source.list.items.indexOf(sourceItem);
-      const targetIndex = Math.max(Math.min(action.targetIndex, targetList.length - 1), 0);
+      const sourceIndex = source.list.items.indexOf(source.item);
+      console.assert(sourceIndex != -1);
+      const targetIndex = Math.max(Math.min(action.targetIndex, targetList.items.length - 1), 0);
 
       source.list.items.splice(sourceIndex, 1);
       targetList.items.splice(targetIndex, 0, source.item);
@@ -1833,9 +1828,10 @@ function foldAction(state, action) {
 // https://stackoverflow.com/a/33486055
 function md5Hash(d){var r = M(V(Y(X(d),8*d.length)));return r.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_}
 
-function rebuildState(id, actions) {
-
-  return actions.reduce(foldAction, { ...emptyState(), id });
+function rebuildStateFromActions(id, actions) {
+  const newState = actions.reduce(foldAction, { ...emptyState(), id });
+  window.lastCommitTime = deserializeDate(newState.time);
+  return newState;
 }
 
 function emptyState() {
