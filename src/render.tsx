57ce1b552de22c0084862a6e58a1e19fa0dfdea3
Michael Hunter <elec.mike@gmail.com>
2021-05-29 07:17:33 +1000

    Refactored and fixed wishlist component

--- src/render.tsx
+++ src/render.tsx
@@ -130,58 +130,71 @@ const CurrencyComponent = ({ amount, decimals, className }: { amount: Currency, 
 const WishListComponent = ({ list }: { list: WishList }) =>
   <div id='current-list' className='list'>
     { /* TODO: attach domDataAttachments for events */ }
-    <div className='list-sticky-area'>
-      <SquirrelGraphic />
-      <MobileTopMenuBar value={undefined} />
-      <div className="list-header">
-        <div className="list-name">
-          <ContentEditable
-            Component={() => <h1 id='list-heading' className='list-heading' />}
-            read={() => getList(list.id).name}
-            write={value => addUserAction({ type: 'ListSetName', listId: list.id, newName: value })}
-            requiresRender={false}
-          />
-        </div>
-        <div className="list-info">
-          <div className="list-allocated">
-            <ContentEditable
-              Component={() => <div className="allocated-amount" />}
-              read={() => formatCurrency(getList(list.id).budget.dollars)}
-              write={v => addUserAction({
-                type: 'ListSetBudget',
-                listId: list.id,
-                budget: { dollars: parseNonNegativeCurrency(v), unit: '/month' }
-              })}
-            />
-            <div className="allocated-unit">{list.budget.unit}</div>
-          </div>
-          {(list.kitty.value !== 0 || list.kitty.rate !== 0) &&
-            <span className="list-overflow">
-                {list.kitty.value >= 0
-                  ? <LinearAmountComponent money={list.kitty}/>
-                  : <LinearAmountComponent
-                      className='debt'
-                      money={{
-                        value: -list.kitty.value,
-                        rate: -list.kitty.rate
-                      }}
-                    />
-                }
-              </span>}
-        </div>
-        <ListMenuComponent />
-        <ol className="purchase-history">
-          {/* TODO */}
-        </ol>
-        <ol className="items-list">
-          {/* TODO */}
-        </ol>
-        <button className="add-item svg-button">
-          {/* TODO: Click handler */}
-          <PlusSvg />
-        </button>
+    <ListHeader list={list} />
+    <ol className="purchase-history">
+      {/* TODO */}
+    </ol>
+    <ol className="items-list">
+      {/* TODO */}
+    </ol>
+    <button className="add-item svg-button">
+      {/* TODO: Click handler */}
+      <PlusSvg />
+    </button>
+  </div>
+
+const ListHeader = ({ list }: { list: WishList }) =>
+  <div className='list-sticky-area'>
+    <SquirrelGraphic />
+    <MobileTopMenuBar value={undefined} />
+    <div className="list-header">
+      <ListName list={list} />
+      <div className="list-info">
+        <ListHeaderAllocated list={list} />
+        {showKitty(list.kitty) && <ListKitty kitty={list.kitty} />}
       </div>
+      <ListMenuComponent />
     </div>
+  </div>
+
+const showKitty = (kitty: LinearAmount) => kitty.value !== 0 || kitty.rate !== 0;
+
+const ListKitty = ({ kitty }: { kitty: LinearAmount }) =>
+  <span className="list-overflow">
+    {kitty.value >= 0
+      ? <LinearAmountComponent money={kitty}/>
+      : <LinearAmountComponent
+          className='debt'
+          money={{
+            value: -kitty.value,
+            rate: -kitty.rate
+          }}
+        />
+    }
+  </span>
+
+const ListHeaderAllocated = ({ list }: { list: WishList }) =>
+  <div className="list-allocated">
+    <ContentEditable
+      Component={() => <div className="allocated-amount" />}
+      read={() => formatCurrency(getList(list.id).budget.dollars)}
+      write={v => addUserAction({
+        type: 'ListSetBudget',
+        listId: list.id,
+        budget: { dollars: parseNonNegativeCurrency(v), unit: '/month' }
+      })}
+    />
+    <div className="allocated-unit">{list.budget.unit}</div>
+  </div>
+
+const ListName = ({ list }: { list: WishList }) =>
+  <div className='list-name'>
+    <ContentEditable
+      Component={() => <h1 id='list-heading' className='list-heading' />}
+      read={() => getList(list.id).name}
+      write={value => addUserAction({ type: 'ListSetName', listId: list.id, newName: value })}
+      requiresRender={false}
+    />
   </div>
 
 const ReadyIndicatorSvg = oldRenderPatternToReact(createReadyIndicatorSvg, ref => <svg ref={ref}/>);
@@ -268,7 +281,7 @@ class LinearChangingMoney extends React.Component<LinearAmountProps, { now: numb
 }
 
 const SquirrelGraphic = () =>
-  <svg xmlns={svgNs} viewBox='0 2 100 100' width='50' height='50' style={{display: 'block'}}>
+  <svg className='squirrel-graphic' xmlns={svgNs} viewBox='0 2 100 100' width='50' height='50' style={{display: 'block'}}>
     <path d='m 57.743013,29.127309 c -12.93795,0.179207 -22.347307,11.920556 -21.895807,24.86346 0.453995,13.014395 12.723204,19.422555 11.922584,33.151853 -0.252254,4.325777 -2.256285,9.132424 -8.96533,14.164208 17.743524,-2.957243 17.743524,-20.700777 17.743524,-35.487045 0,-18.265493 16.265304,-18.27202 22.707897,-8.660942 C 82.21312,36.458046 68.526468,28.977945 57.743013,29.127309 Z M 15.583664,51.653267 c -0.04923,-0.0018 -0.09976,0.0016 -0.151328,0.0098 -1.436303,0.228226 -1.15389,2.04243 -1.331342,4.755288 a 9.8298778,9.8298778 0 0 0 -2.870038,-0.428571 9.8298778,9.8298778 0 0 0 -9.829403,9.829983 9.8298778,9.8298778 0 0 0 9.829403,9.82998 9.8298778,9.8298778 0 0 0 9.829981,-9.82998 9.8298778,9.8298778 0 0 0 -2.327682,-6.351744 c -1.192858,-3.122049 -1.645077,-7.758565 -3.149591,-7.81477 z M 9.2169048,62.582976 a 1.9162314,1.9162314 0 0 1 1.9164392,1.916439 1.9162314,1.9162314 0 0 1 -1.9164392,1.916439 1.9162314,1.9162314 0 0 1 -1.9164393,-1.916439 1.9162314,1.9162314 0 0 1 1.9164393,-1.916439 z m 21.3494092,6.616278 a 16.264895,16.264895 0 0 0 -16.264896,16.264897 16.264895,16.264895 0 0 0 9.497867,14.789739 1.4114845,1.4114845 0 0 0 -0.01155,0.18136 1.4114845,1.4114845 0 0 0 1.41105,1.41162 1.4114845,1.4114845 0 0 0 1.196185,-0.66191 16.264895,16.264895 0 0 0 4.171345,0.54409 A 16.264895,16.264895 0 0 0 46.831211,85.464151 16.264895,16.264895 0 0 0 30.566314,69.199254 Z M 5.3164492,76.690579 A 3.9153737,3.9153737 0 0 0 1.401553,80.606053 3.9153737,3.9153737 0 0 0 5.3164492,84.521526 3.9153737,3.9153737 0 0 0 9.231922,80.606053 3.9153737,3.9153737 0 0 0 5.3164492,76.690579 Z' />
   </svg>
 
